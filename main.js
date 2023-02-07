d4dc148c42371b2efac958532632b11199063a99
Ehsan A. Kian <ehsan_amd72@hotmail.com>
2023-02-07 16:54:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/style/Fonts/Font.css'
-import '@/style/style.scss'
-import '@/style/bootstrap-grid-rtl.css'
 
 Vue.config.productionTip = false
-
-import '@/lib/state'
-import '@/lib/axios'
-import '@/lib/globalConstants'
-import '@/lib/globalFunctions'
 
 new Vue({
   router,
